6fecbeda258735c5819f41a4e43d008a66b78a4a
shishir <shishirbhat2000@gmail.com>
2023-04-25 17:00:18 +0530

    removed dabba code

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,38 +19,15 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Navbar() {
   const [isClicked, setIsClicked] = React.useState(false);
-//   const [state, setState] = React.useState({
-//     top: false,
-//     left: false,
-//     bottom: false,
-//     right: false,
-//   });
-console.log(isClicked);
- 
-function toggleDrawer(value) {
-   setIsClicked(value);
- }
- 
-  
-//   const toggleDrawer = (anchor, open) => (event) => {
-//     if (
-//       event &&
-//       event.type === "keydown" &&
-//       (event.key === "Tab" || event.key === "Shift")
-//     ) {
-//       return;
-//     }
 
-//     setState({ ...state, [anchor]: open });
-//   };
+  console.log(isClicked);
+
+  function toggleDrawer(value) {
+    setIsClicked(value);
+  }
 
   const list = () => (
-    <Box
-      sx={{ width: 'auto'}}
-      role="presentation"
-    //   onClick={toggleDrawer(false)}
-    //   onKeyDown={toggleDrawer(false)}
-    >
+    <Box sx={{ width: "auto" }} role="presentation">
       <List>
         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
           <ListItem key={text} disablePadding>
@@ -90,7 +67,7 @@ function toggleDrawer(value) {
               color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
-              onClick={()=>toggleDrawer(true)}
+              onClick={() => toggleDrawer(true)}
             >
               <MenuIcon />
             </IconButton>
@@ -104,30 +81,9 @@ function toggleDrawer(value) {
       <div>
         {isClicked && (
           <React.Fragment>
-            {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
-            <SwipeableDrawer
-            //   anchor={anchor}
-              open={isClicked}
-            //   onClose={toggleDrawer(false)}
-            //   onOpen={toggleDrawer(false)}
-            >
-              {list()}
-            </SwipeableDrawer>
+            <SwipeableDrawer open={isClicked}>{list()}</SwipeableDrawer>
           </React.Fragment>
         )}
-        {/* {["left", "right", "top", "bottom"].map((anchor) => (
-            <React.Fragment key={anchor}>
-              <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-              <SwipeableDrawer
-                anchor={anchor}
-                open={state[anchor]}
-                onClose={toggleDrawer(anchor, false)}
-                onOpen={toggleDrawer(anchor, true)}
-              >
-                {list(anchor)}
-              </SwipeableDrawer>
-            </React.Fragment>
-          ))} */}
       </div>
     </>
   );
